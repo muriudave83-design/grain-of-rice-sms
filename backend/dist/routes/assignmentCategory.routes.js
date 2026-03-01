6f4908163cfb8643fb103d78686519af8ea2e583
Brian Onesmus <brianwanyoike18@gmail.com>
2026-03-01 23:11:21 +0300

    Fix TypeScript build output and start script

--- backend/dist/routes/assignmentCategory.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const assignmentCategory_controller_1 = require("../controllers/assignmentCategory.controller");
-const router = (0, express_1.Router)();
-router.get("/", authMiddleware_1.authenticate, assignmentCategory_controller_1.listAssignmentCategories);
-exports.default = router;
