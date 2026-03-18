a780ab80a9c82ccb67016b6cf3908650c2459a76
Brian Onesmus <brianwanyoike18@gmail.com>
2026-03-18 21:14:07 +0300

    Add axios interceptor for auth token

--- frontend/src/pages/teacher/gradebook/Gradebook.jsx
+++ frontend/src/pages/teacher/gradebook/Gradebook.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios from "../../../api/axios";
 
 export default function Gradebook() {
   const [data, setData] = useState(null);
