4ec200af3dbf4759a00709683649c1ee1a0796d4
Brian Onesmus <brianwanyoike18@gmail.com>
2026-03-30 13:20:49 +0300

    Add teacher classes page and routing

--- frontend/src/pages/teacher/TeacherClasses.jsx
+++ frontend/src/pages/teacher/TeacherClasses.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "../../api.js";
+import api from "../../api/apiClient";
 
 export default function TeacherClasses() {
   const [classes, setClasses] = useState([]);
