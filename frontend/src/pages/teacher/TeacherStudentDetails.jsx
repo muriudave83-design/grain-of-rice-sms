7bb0cc64f924884abdf22d738200c3744bf03f43
Brian Onesmus <brianwanyoike18@gmail.com>
2026-03-30 16:18:59 +0300

    Add student gradebook page

--- frontend/src/pages/teacher/TeacherStudentDetails.jsx
+++ frontend/src/pages/teacher/TeacherStudentDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import api from "../../api";
+import api from "../../api/apiClient";
 
 export default function TeacherStudentDetails() {
   const { id } = useParams();
